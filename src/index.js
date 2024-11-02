54d6f7450ce6a17f91e958f988fedba1ad5753f2
Sheetal Tripathi <>
2024-11-02 11:22:01 +0530

    updated the code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app" 
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-import React from "react";
-import { render } from "react-dom";
-
-import App from "./App";
-
-render(<App></App>, document.getElementById("app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
